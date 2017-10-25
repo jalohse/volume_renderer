ccd03986bb0c6dc6c45d574b234f230089f98767
Jessica Hair <jessicaalohse@gmail.com>
2017-10-25 14:28:44 -0600

    Remove proxy geo calculations

--- VolumeRenderer/fragment_shader.glsl
+++ VolumeRenderer/fragment_shader.glsl
@@ -6,12 +6,13 @@ out vec4 color;
 
 in vec3 texCoor;
 
-vec3 ambientColor = vec3(0.5, 0, 0);
+vec4 ambientColor = vec4(0.5, 0, 0, 1);
 
 
 void main() {
 	vec4 builtUpColor = vec4(0,0,0, 0);
 	float alpha = 0.0f;
+
 			vec4 texVal = texture(tex, texCoor);
 			if(texVal.r > 0.3 && texVal.r < 0.4) {
 				builtUpColor += vec4(0, 0, 1, 0.2);
@@ -21,6 +22,6 @@ void main() {
 			else if(texVal.r >= 0.6) {
 				builtUpColor += vec4(0, 1, 0, 0.2);
 			}
-	color = builtUpColor;
+	color = ambientColor;
 
 }
--- VolumeRenderer/main.cpp
+++ VolumeRenderer/main.cpp
@@ -9,9 +9,10 @@
 #include <algorithm>
 #include <limits>
 
-#include "nanogui/common.h"
-
-using namespace nanogui;
+//#include "nanogui/common.h"
+//#include "nanogui/formhelper.h"
+//
+//using namespace nanogui;
 
 #define width 300
 #define far_plane 500.0f
@@ -55,15 +56,6 @@ const int LG_VOL_Z = 442;
 
 GLuint texture;
 
-std::vector<cyPoint3f> intersectionPts;
-cyPoint3f planeNormal;
-
-float maxX;
-float minX;
-float maxY;
-float minY;
-float maxZ;
-float minZ;
 
 void setInitialRotationAndTranslation()
 {
@@ -81,196 +73,15 @@ void setInitialRotationAndTranslation()
 	teapotLightMVP = lightProj * lightView * cameraTransformationMatrix;
 }
 
-void addIntersectionPt(cyPoint3f point1, cyPoint3f point2, int& numIntersections, float d, cyPoint3f& center)
-{
-	cyPoint3f normalizedPlane = planeNormal.GetNormalized();
-	float point = (d - normalizedPlane.Dot(point1)) / normalizedPlane.Dot(point2 - point1);
-	if (point >= 0.0 && point <= 1.0)
-	{
-		cyPoint3f intersectionPoint = point1 + point * (point2 - point1);
-		intersectionPts.push_back(intersectionPoint);
-		center += intersectionPoint;
-		numIntersections += 1;
-	}
-}
-
-void findIntersection(cyPoint3f first, cyPoint3f second, cyPoint3f third, cyPoint3f fourth, float d, cyPoint3f& center)
-{
-	int numIntersections = 0;
-	addIntersectionPt(first, second, numIntersections, d, center);
-	if (numIntersections == 0)
-	{
-		addIntersectionPt(second, third, numIntersections, d, center);
-		if (numIntersections == 0)
-		{
-			addIntersectionPt(third, fourth, numIntersections, d, center);
-		}
-	}
-}
-
-bool sortIntersectionPoints(cyPoint3f lhs, cyPoint3f rhs)
-{
-	// algorithm from http://www.asawicki.info/news_1428_finding_polygon_of_plane-aabb_intersection.html
-	const cyPoint3f firstPt = intersectionPts[0];
-	return ((lhs - firstPt).Cross(rhs - firstPt)).Dot(planeNormal) < 0;
-}
-
-void computeProxyGeo(cyPoint4f meshMax, cyPoint4f meshMin, float d, cyPoint3f& center)
-{
-	int numIntersections = 0;
-	std::vector<cyPoint3f> corners = {
-		cyPoint3f(meshMin.x, meshMax.y, meshMin.z), cyPoint3f(meshMin.x, meshMax.y, meshMax.z), meshMin.XYZ(),
-		cyPoint3f(meshMax.x, meshMax.y, meshMin.z),meshMax.XYZ(), cyPoint3f(meshMin.x, meshMin.y, meshMax.z),
-		cyPoint3f(meshMax.x, meshMin.y, meshMin.z), cyPoint3f(meshMax.x, meshMin.y, meshMax.z)
-	};
-	findIntersection(corners[0], corners[1], corners[4], corners[6], d, center);
-	addIntersectionPt(corners[1], corners[5], numIntersections, d, center);
-	findIntersection(corners[0], corners[2], corners[5], corners[7], d, center);
-	addIntersectionPt(corners[2], corners[6], numIntersections, d, center);
-	findIntersection(corners[0], corners[3], corners[6], corners[7], d, center);
-	addIntersectionPt(corners[3], corners[4], numIntersections, d, center);
-	if (intersectionPts.size() != 0)
-	{
-		center = center / intersectionPts.size();
-		std::sort(intersectionPts.begin(), intersectionPts.end(), sortIntersectionPoints);
-	}
-}
-
-void transformPtBackToObjectSpace(cyPoint3f& pt, cyMatrix4f transformationMatrix)
-{
-	cyPoint4f currentPt = cyPoint4f(pt, 1);
-	currentPt = transformationMatrix.GetInverse() * currentPt;
-	if (currentPt.x > 10 || currentPt.x < -10)
-	{
-		if (pt.x == 0)
-		{
-			currentPt.x = 0;
-		}
-		else
-		{
-			float newX = pt.x - 14;
-			currentPt.x = newX;
-		}
-	}
-	if (currentPt.y > 10 || currentPt.y < -10)
-	{
-		if (pt.y == 0)
-		{
-			currentPt.y = 0;
-		}
-		else
-		{
-			float newY = pt.y - 14;
-			currentPt.y = newY;
-		}
-	}
-	if (currentPt.z > 10 || currentPt.z < -10)
-	{
-		if (pt.z == 0)
-		{
-			currentPt.z = 0;
-		}
-		else
-		{
-			float newZ = pt.z - 49.815;
-			currentPt.z = newZ;
-		}
-	}
-
-	pt = cyPoint3f(currentPt);
-}
-
 cyPoint3f getTextureVertexFor(cyPoint3f pt)
 {
 
 	return (pt / 20) + 0.5;
 }
 
-cyPoint3f transformToGlobal(cyPoint3f pt, cyMatrix4f transformationMatrix)
-{
-	cyPoint4f transformedPt = transformationMatrix * cyPoint4f(pt, 1);
-	if (transformedPt.w != 0)
-	{
-		transformedPt = transformedPt / transformedPt.w;
-	}
-	return cyPoint3f(transformedPt);
-}
-
-void sendIntersectionPts(cyPoint3f center, cyMatrix4f transformationMatrix)
-{
-	cyPoint3f centerTextureVertex = getTextureVertexFor(center);
-	for (int i = 0; i < intersectionPts.size(); i++)
-	{
-		cyPoint3f secondPt;
-		if (i == intersectionPts.size() - 1)
-		{
-			secondPt = intersectionPts[0];
-		}
-		else
-		{
-			secondPt = intersectionPts[i + 1];
-		}
-		cyPoint3f firstPt = intersectionPts[i];
-		transformPtBackToObjectSpace(firstPt, transformationMatrix);
-		transformPtBackToObjectSpace(secondPt, transformationMatrix);
-		vertices.push_back(cyPoint3f(firstPt));
-		vertices.push_back(cyPoint3f(secondPt));
-		vertices.push_back(cyPoint3f(center));
-		normals.push_back(planeNormal);
-		normals.push_back(planeNormal);
-		normals.push_back(planeNormal);
-		textureVertices.push_back(getTextureVertexFor(firstPt));
-		textureVertices.push_back(getTextureVertexFor(secondPt));
-		textureVertices.push_back(centerTextureVertex);
-	}
-}
-
-void working()
-{
-	cyMatrix4f transformationMatrix = perspectiveMatrix * view * cameraTransformationMatrix;
-	cyPoint4f transformedMeshMin = transformationMatrix * cyPoint4f(mesh.GetBoundMin(), 1);
-	cyPoint4f transformedMeshMax = transformationMatrix * cyPoint4f(mesh.GetBoundMax(), 1);
-	if (transformedMeshMin.z > transformedMeshMax.z)
-	{
-		std::swap(transformedMeshMax, transformedMeshMin);
-	}
-	cyPoint4f transformedCameraPos = transformationMatrix * cyPoint4f(origin, 1);
-	float volumeDist = transformedMeshMax.z - transformedMeshMin.z;
-	float samplingRate = 25.0;
-	float samplingDistance = volumeDist / samplingRate;
-	float startZ = transformedCameraPos.z;
-	while (startZ < transformedMeshMax.z)
-	{
-		startZ = startZ + samplingDistance;
-		if (startZ > transformedMeshMin.z && startZ < transformedMeshMax.z)
-		{
-			intersectionPts = {};
-
-			cyPoint3f hitPoint = transformedCameraPos.XYZ() + (startZ * -transformedCameraPos.XYZ().GetNormalized());
-			planeNormal = -transformedCameraPos.XYZ();
-			float d = planeNormal.x * hitPoint.x + planeNormal.y * hitPoint.y + planeNormal.z * hitPoint.z;
-			float distance = d / sqrt(
-				planeNormal.x * planeNormal.x + planeNormal.y * planeNormal.y + planeNormal.z * planeNormal.z);
-			cyPoint3f center = cyPoint3f(0, 0, 0);
-			computeProxyGeo(transformedMeshMax, transformedMeshMin, distance, center);
-			transformPtBackToObjectSpace(center, transformationMatrix);
-			sendIntersectionPts(center, transformationMatrix);
-		}
-	}
-}
-
-void addSlicesToVBO()
-{
-	working();
-}
-
 void display()
 {
-	vertices.clear();
-	normals.clear();
-	textureVertices.clear();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	addSlicesToVBO();
 	volume_shaders.Bind();
 	glBindVertexArray(vertexArrayObj);
 	glDrawArrays(GL_TRIANGLES, 0, vertices.size());
@@ -302,7 +113,6 @@ void rotate()
 	totalRotationMatrix = cyMatrix4f::MatrixRotationX(0.5) * totalRotationMatrix;
 	cameraTransformationMatrix = translationMatrix * totalRotationMatrix;
 	volume_shaders.SetUniform(1, cameraTransformationMatrix);
-	volume_shaders.SetUniform(3, cameraTransformationMatrix.GetInverse().GetTranspose());
 	glutPostRedisplay();
 }
 
@@ -360,6 +170,20 @@ void reset(int key, int x, int y)
 	}
 }
 
+void populateVerticesAndNormals() {
+	vertices = {};
+	for (int i = 0; i < mesh.NF(); i = i + 1) {
+		cy::TriMesh::TriFace face = mesh.F(i);
+		vertices.push_back(mesh.V(face.v[0]));
+		vertices.push_back(mesh.V(face.v[1]));
+		vertices.push_back(mesh.V(face.v[2]));
+		cy::TriMesh::TriFace nface = mesh.FN(i);
+		normals.push_back(mesh.VN(nface.v[0]).GetNormalized());
+		normals.push_back(mesh.VN(nface.v[1]).GetNormalized());
+		normals.push_back(mesh.VN(nface.v[2]).GetNormalized());
+	}
+}
+
 
 void createObj()
 {
@@ -370,7 +194,7 @@ void createObj()
 	mesh.ComputeNormals();
 
 	setInitialRotationAndTranslation();
-	addSlicesToVBO();
+	populateVerticesAndNormals();
 
 	volume_shaders = cy::GLSLProgram();
 	volume_shaders.BuildFiles("vertex_shader.glsl", "fragment_shader.glsl");
@@ -399,6 +223,11 @@ void createObj()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cyPoint3f) * normals.size(), &normals[0], GL_STATIC_DRAW);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, 0, sizeof(cyPoint3f), NULL);
+
+	for(int i = 0; i < vertices.size(); i++)
+	{
+		textureVertices.push_back(getTextureVertexFor(vertices.at(i)));
+	}
 
 	glGenBuffers(1, textureBufferObj);
 	glBindBuffer(GL_ARRAY_BUFFER, textureBufferObj[0]);
@@ -462,6 +291,7 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
 		return 1;
 	}
+
 
 	float fov = fov_degrees * (M_PI / 180.0f);
 	perspectiveMatrix = cyMatrix4f::MatrixPerspective(fov, 1.0f, 0.1f, far_plane);
